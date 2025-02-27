7b4f9b54eb5972926525c3fd935fa1d6ee49b384
Josephtantillo6 <joseph.s.tantillo@gmail.com>
2025-02-26 20:56:53 -0500

    Update clustering.R
    fix file

--- kmeans/R/clustering.R
+++ kmeans/R/clustering.R
@@ -11,17 +11,3 @@ perform_kmeans <- function(data, xvar, yvar, centers = 3) {
   selected_data <- data[, c(xvar, yvar)]
   kmeans(selected_data, centers)
 }
-
-#' Plot K-Means Clusters
-#'
-#' @param data A data frame.
-#' @param kmeans_result A kmeans object.
-#' @param xvar X variable name.
-#' @param yvar Y variable name.
-#'
-#' @return A scatter plot.
-#' @export
-plot_kmeans <- function(data, kmeans_result, xvar, yvar) {
-  plot(data[, c(xvar, yvar)], col = kmeans_result$cluster, pch = 20, cex = 3)
-  points(kmeans_result$centers, pch = 4, cex = 4, lwd = 4, col = "black")
-}
